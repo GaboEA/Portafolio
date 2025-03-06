bdfa09fcbf2118479717f60f4998fdfd768c59d6
Areyouthere003 <jgerazo7@misena.edu.co>
2025-03-06 01:14:36 -0500

    updated 'StickyNav' and changed nav functionality

--- js/stickyNav.js
+++ js/stickyNav.js
@@ -2,14 +2,16 @@
     var numero = 0;
     var detectorPos = true;
 
+    const douc = "repeating-linear-gradient(rgb(4, 5, 43),rgb(39, 41, 80) )";
     const src = document.getElementById("--box");
+    
     src.addEventListener("touchstart",(e) => {
           for (let i = 0; i < e.touches.length; i++) {
             console.log(`touchpoint[${i}].screenY = ${e.touches[i].screenY}`);
           }
         },
         false,
-      );    
+    );    
 
     window.addEventListener("scroll", (event) => {
         let scroll = this.scrollY;
@@ -23,10 +25,17 @@
                 
                 document.getElementById("--nav_bg").style.position = "static";
                 document.getElementById("--nav_bg").style.top = "0px";
-                document.getElementById("--nav_bg").style.background = "repeating-linear-gradient(rgb(4, 5, 43),rgb(39, 41, 80) )";
+                document.getElementById("--nav_bg").style.background = 
+                "repeating-linear-gradient(rgb(4, 5, 43),rgb(39, 41, 80) )";
                 document.getElementById("--nav_bg").style.transition = "ease-in-out";
-                document.getElementById("--nav_bg").style.transitionDuration = "1s";
+                document.getElementById("--nav_bg").style.transitionDuration = "0.3s";
                 document.getElementById("--nav_bg").style.background = "transparent";
+                document.getElementById("--nav_bg").style.backgroundColor = "#04052b";
+                document.getElementById("--nav_bg").style.backgroundImage = 
+                "radial-gradient(#e5e5f730 0.5px, #04052b 0.5px)";
+                document.getElementById("--nav_bg").style.backgroundSize = "10px 10px";
+
+
             }
         } 
         else if (numero >= 100){
@@ -38,17 +47,21 @@
                 document.getElementById("--nav_bg").style.background = "transparent";
                 document.getElementById("--nav_bg").style.backgroundColor = "transparent";
                 document.getElementById("--nav_bg").style.transition = "ease-in-out";
-                document.getElementById("--nav_bg").style.transitionDuration = "1s";
+                document.getElementById("--nav_bg").style.transitionDuration = "0.3s";
                 document.getElementById("--nav_bg").style.backgroundColor = "rgb(4, 5, 43)";
                 
-                var douc = /*document.getElementById("--nav_bg").style.backgroundImage =*/ "repeating-linear-gradient(rgb(4, 5, 43),rgb(39, 41, 80) )";
-    
                 function wait4Seconds() {
                     return new Promise((resolve) => {
                         setTimeout(() => {
                             resolve(douc);
-                            document.getElementById("--nav_bg").style.backgroundImage = douc;
-                        }, 1000);
+                            if(!detectorPos)
+                            {
+                                document.getElementById("--nav_bg").style.backgroundImage = douc;
+                            }
+                            else{
+                                document.getElementById("--nav_bg").style.background = "transparent";
+                            }
+                        }, 300);
                     });
                 }
                 
